275459c35abb62176b98cb52e636fa6714d73ebf
hungnvhd <39705239+hungnvhd@users.noreply.github.com>
2018-06-10 03:18:47 +0700

    hung nguyen

--- FParkingOwners/app/src/main/java/com/example/hung/fparkingowners/Statistical.java
+++ FParkingOwners/app/src/main/java/com/example/hung/fparkingowners/Statistical.java
@@ -1,8 +1,11 @@
 package com.example.hung.fparkingowners;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
@@ -11,41 +14,69 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class Statistical extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
-    TextView textViewFromDate, textViewToDate;
-    ImageView fromDate, toDate;
+public class Statistical extends AppCompatActivity {
+    private static final String TAG = "Statistical";
+
+    private TextView textViewFromDate, textViewToDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener1;
+    private DatePickerDialog.OnDateSetListener mDateSetListener2;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistical);
         getSupportActionBar().hide();
-        fromDate = (ImageView) findViewById(R.id.imgFromDate);
-        toDate = (ImageView) findViewById(R.id.imgToDate);
-        textViewFromDate =(TextView) findViewById(R.id.txtFromDate);
-        textViewToDate =(TextView) findViewById(R.id.txtToDate);
-        fromDate.setOnClickListener(new View.OnClickListener() {
+        textViewFromDate = findViewById(R.id.txtFromDate);
+        textViewToDate = findViewById(R.id.txtToDate);
+        textViewToDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerFragment datePicker = new DatePickerFragment();
-                datePicker.show(getSupportFragmentManager(), "date picker");
-            }
-        });
-        toDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatePickerFragment datePicker2 = new DatePickerFragment();
-                datePicker2.show(getSupportFragmentManager(), "date picker");
-            }
-        });
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Statistical.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener2, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
 
-    }
-@Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String currentDateString = DateFormat.getDateInstance(DateFormat.DEFAULT).format(calendar.getTime());
+            }
+        });
+        textViewFromDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Statistical.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener1, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+        mDateSetListener1 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: yyy/mm/dd: " + year + "-" + month + "-" + day);
+
+                String date = year + "-" + month + "-" + day;
+                textViewFromDate.setText(date);
+            }
+        };
+        mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: yyy/mm/dd: " + year + "-" + month + "-" + day);
+
+                String date = year + "-" + month + "-" + day;
+                textViewToDate.setText(date);
+            }
+        };
     }
 }
