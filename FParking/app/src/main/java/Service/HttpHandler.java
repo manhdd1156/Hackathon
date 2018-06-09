c5ce034f0fb46903bc3081e55cb13979d92d79a5
Hung <hungbqse04029@fpt.edu.vn>
2018-06-09 09:23:53 +0700

    Hung Bui Commit

--- FParking/app/src/main/java/Service/HttpHandler.java
+++ FParking/app/src/main/java/Service/HttpHandler.java
@@ -1,13 +1,16 @@
 package Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpHandler {
-    URL url = null;
+
+    String result = null;
 
     public String getrequiement(String requiementURL) {
 
@@ -16,7 +19,7 @@ public class HttpHandler {
 
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.connect();
-            
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -26,7 +29,11 @@ public class HttpHandler {
         return "ahih";
     }
 
-    public String convertStremToString(InputStream inputStream){
-        return "ahihi";
+    public String convertStremToString(InputStream inputStream) {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        
+        return sb;
     }
 }
