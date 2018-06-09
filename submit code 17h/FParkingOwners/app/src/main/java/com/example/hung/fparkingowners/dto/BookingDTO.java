87c7d1bdd76bd7b9b493e132aee4a02e996ffcf2
Duy Manh <manhdd1156@gmail.com>
2018-06-09 16:47:51 +0700

    submit code 17h

---
+++ submit code 17h/FParkingOwners/app/src/main/java/com/example/hung/fparkingowners/dto/BookingDTO.java
@@ -0,0 +1,101 @@
+package com.example.hung.fparkingowners.dto;
+
+public class BookingDTO {
+    private int bookingID;
+    private int parkingID;
+    private int carID;
+    private String status;
+    private String checkinTime;
+    private String checkoutTime;
+    private String licensePlate;
+    private String typeCar;
+    private double price;
+
+    public BookingDTO() {
+
+    }
+
+    public BookingDTO(int bookingID, int parkingID, int carID, String status, String checkinTime, String checkoutTime, String licensePlate, String typeCar, double price) {
+        this.bookingID = bookingID;
+        this.parkingID = parkingID;
+        this.carID = carID;
+        this.status = status;
+        this.checkinTime = checkinTime;
+        this.checkoutTime = checkoutTime;
+        this.licensePlate = licensePlate;
+        this.typeCar = typeCar;
+        this.price = price;
+    }
+
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public int getParkingID() {
+        return parkingID;
+    }
+
+    public int getCarID() {
+        return carID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCheckinTime() {
+        return checkinTime;
+    }
+
+    public String getCheckoutTime() {
+        return checkoutTime;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getTypeCar() {
+        return typeCar;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public void setParkingID(int parkingID) {
+        this.parkingID = parkingID;
+    }
+
+    public void setCarID(int carID) {
+        this.carID = carID;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setCheckinTime(String checkinTime) {
+        this.checkinTime = checkinTime;
+    }
+
+    public void setCheckoutTime(String checkoutTime) {
+        this.checkoutTime = checkoutTime;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public void setTypeCar(String type) {
+        this.typeCar = typeCar;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
