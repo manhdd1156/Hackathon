eac4efaeec20a09420672c48e68baa52c33f949b
KhoiND <khoindse04028@fpt.edu.vn>
2018-06-09 13:22:19 +0700

    CheckOut Update

--- FParking/app/src/main/java/com/example/hung/fparking/CheckOut.java
+++ FParking/app/src/main/java/com/example/hung/fparking/CheckOut.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,28 +15,47 @@ import Service.HttpHandler;
 
 public class CheckOut extends AppCompatActivity {
 
+    TextView textViewAddress, textViewCheckIn, textViewPrice, textViewLicensePlate;
+
+    double selectPlaceLat = 0;
+    double selectPlaceLng = 0;
+    String strJSON;
+    String address = "N/A";
+    int currentSpace = 0;
+    int totalSlot = 0;
+    double price = 0;
+    String timeCheckIN = "N/A";
+    String licensePlate = "N/A";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
         getSupportActionBar().hide();
+
+        textViewAddress = findViewById(R.id.textViewAddress);
+        textViewCheckIn = findViewById(R.id.textViewCheckinTime);
+        textViewPrice = findViewById(R.id.textViewPrice);
+        textViewLicensePlate = findViewById(R.id.textViewLicensePlate);
+
+        new GetCheckOutInfor().execute();
     }
 
-    public class GetDetailParking extends AsyncTask<Void, Void, Void> {
+    public class GetCheckOutInfor extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
 
             HttpHandler httpHandler = new HttpHandler();
 
-            Intent intentGetParkingLocation = getIntent();
-            String[] getParkingLocation = getLat_lng(intentGetParkingLocation.getStringExtra("ParkingLocation"));
-
-            selectPlaceLat = Double.parseDouble(getParkingLocation[0]);
-            selectPlaceLng = Double.parseDouble(getParkingLocation[1]);
+//            Intent intentGetParkingLocation = getIntent();
+//            String[] getParkingLocation = getLat_lng(intentGetParkingLocation.getStringExtra("ParkingLocation"));
+//
+//            selectPlaceLat = Double.parseDouble(getParkingLocation[0]);
+//            selectPlaceLng = Double.parseDouble(getParkingLocation[1]);
 
             Log.e("GetNearPlace:", "O day");
-            strJSON = httpHandler.getrequiement("https://fparking.net/realtimeTest/driver/get_Detail_Parking.php?latitude=" + selectPlaceLat + "&" + "longitude=" + selectPlaceLng);
+            strJSON = httpHandler.getrequiement("https://fparking.net/realtimeTest/driver/get_CheckOut_Detail.php?bookingID=143");
             Log.e("SQL Detail:", strJSON.toString());
 
             if (strJSON != null) {
@@ -49,13 +69,9 @@ public class CheckOut extends AppCompatActivity {
                         JSONObject c = jsonArrayParking.getJSONObject(i);
 
                         address = c.getString("address");
-                        currentSpace = c.getInt("currentSpace");
-                        totalSlot = c.getInt("space");
                         price = c.getDouble("price");
-                        time = c.getString("timeoc");
-                        parkingLatitde = c.getDouble("latitude");
-                        parkinglongitude = c.getDouble("longitude");
-
+                        timeCheckIN = c.getString("checkinTime");
+                        licensePlate = c.getString("licensePlate");
                     }
                 } catch (JSONException e) {
                     Log.e("JSONException:", e.getMessage());
@@ -74,12 +90,15 @@ public class CheckOut extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            textViewEmptySpace.setText(totalSlot - currentSpace + "");
-            textViewSlots.setText("/" + totalSlot + "");
-            textViewPrice.setText(price + "");
-            textViewTime.setText(time);
             textViewAddress.setText(address);
+            textViewCheckIn.setText(timeCheckIN);
+            textViewPrice.setText(price+"");
+            textViewLicensePlate.setText(licensePlate);
         }
 
+    }
+    public String[] getLat_lng(String location) {
+        String[] latlng = location.substring(location.indexOf("(") + 1, location.indexOf(")")).split(",");
+        return latlng;
     }
 }
